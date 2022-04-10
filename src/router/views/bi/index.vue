1cc0860a2861f120d2d4509494b99842f4470662
Joao Victor <joao.dvlp@gmail.com>
2022-04-10 18:24:15 -0300

    list all BIs in table

--- src/router/views/bi/index.vue
+++ src/router/views/bi/index.vue
@@ -22,24 +22,27 @@ export default {
   },
   data() {
     return {
-      urlBi: 'https://app.powerbi.com/view?r=' + this.$route.params.sufixLink,
-      title: "Dashboard",
+      title: "Business Intelligence",
       items: [
         {
-          text: "Dashboard",
+          text: "Business Intelligence",
           href: "/",
         },
         {
-          text: "Default",
+          text: "index",
           active: true,
         },
       ],
-      
+        businessIntelligencies: []
     };
   },
   mounted() {
-  // eslint-disable-next-line no-console
-    console.log('this.$route.params.sufixLink')
+    const vm = this
+    this.$http.get('businessIntelligence/', {headers: {'Authorization': 'Bearer ' + sessionStorage.getItem('userToken')}}).then(response => {
+        vm.businessIntelligencies = response.data;
+        // eslint-disable-next-line no-console
+        console.log(response.data)
+    });
   }
 };
 </script>
@@ -47,7 +50,48 @@ export default {
 <template>
   <Layout>
     <PageHeader :title="title" :items="items" />
-    
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title mb-4">Formulario</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title mb-4">Cadastrados</h4>
+            <div class="table-responsive mt-4">
+              <table class="table table-centered">
+                <thead>
+                  <th>Titulo</th>
+                  <th>Descricao</th>
+                  <th>Acoes</th>
+                </thead>
+                <tbody>
+                  <tr v-for="(bi, idx) in businessIntelligencies" :key="idx">
+                    <td>
+                      <p class="mb-0">{{bi.title}}</p>
+                    </td>
+                    <td>
+                      <p class="mb-0">{{bi.description}}</p>
+                    </td>
+                    <td>
+                      <p class="mb-0">{{bi.createdAt}}</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
   
 </template>
